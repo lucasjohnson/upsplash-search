35ab3e6cb2efe3d8223ac1e7e7401dbe3dd97ad9
Lucas Johnson <contact@lucasjohnson.co.nz>
2021-08-16 22:32:58 +0200

    created results title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,8 +6,9 @@ import Image from "./Image";
 import Copy from "../json/copy.json";
 
 const App: React.FC = () => {
-  const [images, setImages] = useState<Array<object>>([{}]);
+  const [images, setImages] = useState<Array<object>>([]);
   const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<number>(0);
   const queryUrl: string = `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_CLIENT_ID}&query=${query}&page=1&per_page=20`;
 
   const fetchImages = () => {
@@ -16,7 +17,9 @@ const App: React.FC = () => {
         headers: {}
       })
       .then(response => {
+        console.log(response);
         setImages(response.data.results);
+        setResults(response.data.total);
       })
       .catch(error => {
         console.warn(error);
@@ -26,7 +29,8 @@ const App: React.FC = () => {
   const handleImageSearch = (event: KeyboardEvent): void => {
     if (event?.keyCode === KeyCode.ENTER) {
       setQuery((event.target as HTMLInputElement).value);
-      setImages([{}]);
+      setImages([]);
+      setResults(0);
     }
   };
 
@@ -41,20 +45,25 @@ const App: React.FC = () => {
         <Input setKeyDown={handleImageSearch} />
       </header>
       <main className="Main">
-        {images.length > 1 && (
-          <ul className="imageItems">
-            {images.map((image: any, key: number) => (
-              <li className="imageItem" key={key}>
-                <Image
-                  imageDesktop={image.urls.regular}
-                  imageMobile={image.urls.small}
-                  description={image.alt_description}
-                  author={image.user.name}
-                  url={image.links.html}
-                />
-              </li>
-            ))}
-          </ul>
+        {query.length > 0 && (
+          <h2 className="resultsTitle">{`${results} ${Copy.results} '${query}'`}</h2>
+        )}
+        {images.length > 0 && (
+          <React.Fragment>
+            <ul className="imageItems">
+              {images.map((image: any, key: number) => (
+                <li className="imageItem" key={key}>
+                  <Image
+                    imageDesktop={image.urls.regular}
+                    imageMobile={image.urls.small}
+                    description={image.alt_description}
+                    author={image.user.name}
+                    url={image.links.html}
+                  />
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
         )}
       </main>
       <footer className="Footer"></footer>
