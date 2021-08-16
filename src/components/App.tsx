3512bd4f9521898fd007ff04e68708955594b531
Lucas Johnson <contact@lucasjohnson.co.nz>
2021-08-16 22:42:15 +0200

    set loading state icon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FaReact } from "react-icons/fa";
 import { KeyCode } from "../enums/Index";
 import Input from "./Input";
 import Image from "./Image";
@@ -9,6 +10,7 @@ const App: React.FC = () => {
   const [images, setImages] = useState<Array<object>>([]);
   const [query, setQuery] = useState<string>("");
   const [results, setResults] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
   const queryUrl: string = `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_CLIENT_ID}&query=${query}&page=1&per_page=20`;
 
   const fetchImages = () => {
@@ -17,9 +19,9 @@ const App: React.FC = () => {
         headers: {}
       })
       .then(response => {
-        console.log(response);
         setImages(response.data.results);
         setResults(response.data.total);
+        setLoading(false);
       })
       .catch(error => {
         console.warn(error);
@@ -35,6 +37,7 @@ const App: React.FC = () => {
   };
 
   useEffect(() => {
+    setLoading(true);
     fetchImages();
   }, [query]);
 
@@ -43,6 +46,11 @@ const App: React.FC = () => {
       <header className="Header">
         <h1 className="title">{Copy.title}</h1>
         <Input setKeyDown={handleImageSearch} />
+        {loading && (
+          <div className="loading" aria-busy={loading}>
+            <FaReact />
+          </div>
+        )}
       </header>
       <main className="Main">
         {query.length > 0 && (
